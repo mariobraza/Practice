a6cc9a9dcca884168ec94babdf002d9df1ec51e9
mariobraza <100727097+mariobraza@users.noreply.github.com>
2022-07-08 18:13:28 +0000

    comeback

---
+++ modules/lesson5_2.py
@@ -0,0 +1,8 @@
+x = int(input('Введите координату Х: '))
+y = int(input('Введите координату Y: '))
+if x > y:
+  print('Х больше Y')
+if x == y:
+  print('Х равен Y')
+if x < y:
+  print('Х меньше Y')
