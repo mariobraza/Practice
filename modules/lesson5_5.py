9d258549c67b119efe6db97adaf095a6eb7e1ef4
mariobraza <100727097+mariobraza@users.noreply.github.com>
2022-07-13 23:34:05 +0000

    while

--- modules/lesson5_5.py
+++ modules/lesson5_5.py
@@ -66,3 +66,50 @@ if (10 > answer >= 1) or (-1 >= answer > -10):
   print('Двухзначное')
 else:
   print('Не двухначное')
+
+
+
+
+
+x = int(input('1: '))
+y = int(input('2: '))
+z = int(input('3: '))
+if x == y == z:
+  print('3 совпадения')
+elif x == y != z or x != y == z or x == z !=y:
+  print('2 совпадения')
+else:
+  print ('Нет совпадений')
+
+
+
+
+area = int(input('Введите площадь квартиры: '))
+cost = int(input('Введите стоимость квартиры: '))
+if area >= 100 and cost <= 10000000 or area >= 80 and cost <= 7000000:
+  print('Квартира подходит!')
+else:
+  print('Квартира НЕ подходит!')
+
+
+
+
+profit = int(input('Введите сумму прибыли: '))
+if profit < 0:
+  print('Ошибка: доход не может быть отрицательным')
+else:
+  if profit < 10000:
+    tax13 = profit * 13 / 100
+    print('Ставка налога (13%): ', tax13)
+  elif profit < 50000:
+    tax20 = ((profit-10000) * 20 / 100) + (10000 * 13 / 100) 
+    print('Ставка налога (20%): ', tax20)
+  else:
+    tax30 = ((profit-50000) * 30 / 100) + (40000 * 20 / 100) + (10000 * 13 / 100) 
+    print('Ставка налога (30%): ', tax30)    
+
+time = int(input('Введите текущее время в формате ЧЧ: '))
+if (time >= 8 and time < 10) or (time >= 12 and time < 14) or (time >= 15) and (time < 18) or (time >= 20 and time < 22):
+  print('Можно получить посылку')
+else:
+  print('Посылку получить нельзя')
